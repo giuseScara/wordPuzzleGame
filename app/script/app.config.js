14a4b7c6db6a8a8d422164682582c8b451dff893
Scaramuzzino Giuseppe <giuse.scara@gmail.com>
2016-12-05 14:28:28 +0100

    Modify gulp task. Add ext_libs to deploy

--- app/script/app.config.js
+++ app/script/app.config.js
@@ -1,10 +1,10 @@
-var pathExtLibs = "../bower_components/";
+var pathExtLibs = "../app/ext_libs/";
 
 require.config({
   baseUrl: ".",
   map: {
     '*': {
-      'css': pathExtLibs + '/require-css/css.min'
+      'css': pathExtLibs + '/css.min/css.min'
     }
   },
 
@@ -19,21 +19,21 @@ require.config({
   paths: {
     'angular': [
       'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.8/angular.min',
-      pathExtLibs + 'angular/angular.min'
+      pathExtLibs + 'angular.min/angular.min'
     ],
     'angular-route': [
       'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.8/angular-route.min',
-      pathExtLibs + 'angular-route/angular-route.min'
+      pathExtLibs + 'angular-route.min/angular-route.min'
     ],
     'angular-timer': [
       'https://cdnjs.cloudflare.com/ajax/libs/angular-timer/1.3.4/angular-timer.min',
-      pathExtLibs + 'angular-timer/dist/angular-timer.min'
+      pathExtLibs + 'angular-timer.min/angular-timer.min'
     ],
     'firebase': [
       'https://www.gstatic.com/firebasejs/3.6.1/firebase',
       pathExtLibs + 'firebase/firebase'
     ],
-    'moment': pathExtLibs + 'moment/min/moment.min',
+    'moment': pathExtLibs + 'moment.min/moment.min',
     'humanize-duration': pathExtLibs + 'humanize-duration/humanize-duration',
     'app': '../app/script/app',
     'app.route': '../app/script/app.route',
--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,98 @@
-var gulp = require('gulp');
-var bower = require("gulp-bower");
-var clean = require('gulp-clean');
-var watchLess = require('gulp-watch-less');
-var less = require('gulp-less');
-var path = require('path');
-var jshint = require('gulp-jshint');
-var imagemin = require('gulp-imagemin');
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    changed = require('gulp-changed'),
+    rev = require('gulp-rev'),
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    bower = require("gulp-bower");
 
-gulp.task('default', ['bower'], function () {
-
-});
-
-gulp.task('lint', function () {
-  return gulp.src('js/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default', {
-      verbose: true
-    }));
-});
+var pathExtLib = "./bower_components/",
+    extLibs = "app/ext_libs/";
 
 //download libs
 gulp.task('bower', function () {
   return bower();
 });
 
-//from less to css
-gulp.task('less', function () {
-  gulp.src('**/**/*.less')
-    .pipe(less())
-    .pipe(gulp.dest(function (file) {
-      return file.base;
-    }));
+
+gulp.task('jshint', function() {
+  return gulp.src(['app/scripts/**/*.js', 'app/view/**/*.js'])
+  .pipe(jshint())
+  .pipe(jshint.reporter(stylish));
 });
 
-//reduce size img
-gulp.task('img', function () {
-  return gulp.src('img/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('img'));
+// Clean
+gulp.task('clean', function() {
+    return del([extLibs], { force: true });
 });
 
-gulp.task('styles', function () {
-  gulp.src('css/*.css')
-    .pipe(autoprefixer())
-    .pipe(size())
-    .pipe(gulp.dest('css/styles.css'));
+gulp.task('lib_js', function () {
+  var libs = [
+    pathExtLib + 'angular/angular.min.js',
+    pathExtLib + 'angular-route/angular-route.min.js',
+    pathExtLib + 'jquery/dist/jquery.min.js',
+    pathExtLib + 'requirejs/require.js',
+    pathExtLib + 'angular-timer/dist/angular-timer.min',
+    pathExtLib + 'bootstrap/dist/js/bootstrap.min.js',
+    pathExtLib + 'firebase/firebase.js',
+    pathExtLib + 'moment/min/moment.min.js',
+    pathExtLib + 'humanize-duration/humanize-duration.js',
+    pathExtLib + 'require-css/css.min.js'
+    ];
+
+   return gulp.src(libs)
+      .pipe(rename(function (path) {
+        path.dirname += "/"+ path.basename;
+        return path;
+      }))
+      .pipe(gulp.dest(extLibs));
+});
+
+gulp.task('lib_css', function () {
+  var libs = [
+    pathExtLib + 'bootstrap/dist/css/bootstrap.min.css',
+    pathExtLib + 'bootstrap/dist/css/bootstrap.min.css.map'
+
+    ];
+
+   return gulp.src(libs)
+      .pipe(rename(function (path) {
+        path.dirname += "/"+ path.basename;
+        return path;
+      }))
+      .pipe(gulp.dest(extLibs));
+});
+
+// Default task
+gulp.task('default', ['clean', 'bower'], function() {
+    gulp.start('jshint', 'lib_js','lib_css');
+});
+
+
+gulp.task('browser-sync', ['default'], function () {
+   var files = [
+      'app/view/**/*.*',
+      'app/styles/**/*.css',
+      'app/images/**/*.jpg',
+      'app/scripts/**/*.js'
+   ];
+
+   browserSync.init(files, {
+      server: {
+         baseDir: "app",
+         index: "index.html"
+      }
+   });
+        // Watch any files in dist/, reload on change
+  gulp.watch(['app/**']).on('change', browserSync.reload);
+
 });
